9113a6d293889f2aabc6a3f2e0e2341f3cb191da
fauzie primarka <fauzieprimarka@gmail.com>
2025-06-12 15:52:18 +0700

    menambahkan void insert struktur kode

--- Queues.cpp
+++ Queues.cpp
@@ -15,4 +15,10 @@ class Queues
             REAR = -1;
         }
 
-    };
+        void insert()
+        {
+            int num;
+            cout << "Enter a number:" ;
+            cin >> num;
+            cout << endl;
+            }    };
