61ed8caae062d236bbac88ffc227a1d98c650b08
fauzie primarka <fauzieprimarka@gmail.com>
2025-06-12 15:58:47 +0700

    jika REAR berada di posisi terakhir array, kembali ke awal array

--- Queues.cpp
+++ Queues.cpp
@@ -35,4 +35,14 @@ class Queues
                 FRONT = 0;
                 REAR = 0;
             }
+            else 
+            {
+                //jika REAR berada di posisi terakhir array, kembali ke awal array
+                if (REAR == MAX - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+
+            }
+            queues_array[REAR] = num;
         }};
